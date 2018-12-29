f2696f717427eabbd3d748458821a0c2f1a25783
Chao Xu <xuchao@google.com>
2018-12-28 17:30:03 -0800

    enable logs in initializer and the migrator

--- cmd/migrator/migrator.go
+++ cmd/migrator/migrator.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	goflag "flag"
 	"fmt"
 	"os"
 
+	"github.com/golang/glog"
 	"github.com/kubernetes-sigs/kube-storage-version-migrator/cmd/migrator/app"
+	"github.com/spf13/pflag"
 )
 
 func main() {
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	pflag.Parse()
+	pflag.VisitAll(func(flag *pflag.Flag) {
+		glog.V(4).Infof("FLAG: --%s=%q", flag.Name, flag.Value)
+	})
 	command := app.NewInitializerCommand()
 	if err := command.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
