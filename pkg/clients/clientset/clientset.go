96405361eab0a7211f32becf94099455679bc3f2
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-03-12 10:55:22 +0100

    Update codegen

--- pkg/clients/clientset/clientset.go
+++ pkg/clients/clientset/clientset.go
@@ -19,6 +19,8 @@ limitations under the License.
 package clientset
 
 import (
+	"fmt"
+
 	migrationv1alpha1 "github.com/kubernetes-sigs/kube-storage-version-migrator/pkg/clients/clientset/typed/migration/v1alpha1"
 	discovery "k8s.io/client-go/discovery"
 	rest "k8s.io/client-go/rest"
@@ -28,8 +30,6 @@ import (
 type Interface interface {
 	Discovery() discovery.DiscoveryInterface
 	MigrationV1alpha1() migrationv1alpha1.MigrationV1alpha1Interface
-	// Deprecated: please explicitly pick a version if possible.
-	Migration() migrationv1alpha1.MigrationV1alpha1Interface
 }
 
 // Clientset contains the clients for groups. Each group has exactly one
@@ -44,12 +44,6 @@ func (c *Clientset) MigrationV1alpha1() migrationv1alpha1.MigrationV1alpha1Inter
 	return c.migrationV1alpha1
 }
 
-// Deprecated: Migration retrieves the default version of MigrationClient.
-// Please explicitly pick a version.
-func (c *Clientset) Migration() migrationv1alpha1.MigrationV1alpha1Interface {
-	return c.migrationV1alpha1
-}
-
 // Discovery retrieves the DiscoveryClient
 func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 	if c == nil {
@@ -59,9 +53,14 @@ func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 }
 
 // NewForConfig creates a new Clientset for the given config.
+// If config's RateLimiter is not set and QPS and Burst are acceptable,
+// NewForConfig will generate a rate-limiter in configShallowCopy.
 func NewForConfig(c *rest.Config) (*Clientset, error) {
 	configShallowCopy := *c
 	if configShallowCopy.RateLimiter == nil && configShallowCopy.QPS > 0 {
+		if configShallowCopy.Burst <= 0 {
+			return nil, fmt.Errorf("burst is required to be greater than 0 when RateLimiter is not set and QPS is set to greater than 0")
+		}
 		configShallowCopy.RateLimiter = flowcontrol.NewTokenBucketRateLimiter(configShallowCopy.QPS, configShallowCopy.Burst)
 	}
 	var cs Clientset
